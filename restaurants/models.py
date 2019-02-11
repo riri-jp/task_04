a8bd4fc366cf947601a1bf20ee0bd91b9149841b
riri-jp <alahmary.mashari@gmail.com>
2019-02-11 10:42:07 +0300

    shaafal_mashari

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    opening_time =  models.TimeField(auto_now_add=True)
+    closing_time = models.TimeField(auto_now_add=True)
+
+
+
+
+    def __str__(self):
+            return self.name
